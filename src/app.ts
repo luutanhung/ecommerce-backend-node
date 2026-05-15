5ff3d1be4afee18aacfe240579f005d93ab49004
luutanhung <luutanhung.dev@gmail.com>
2026-05-15 07:58:05 +0700

    chore: add docs folder to store documentation

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,12 @@
 import type { Express } from "express";
 import express from "express";
+import morgan from "morgan";
 
 const app: Express = express();
+
+/**
+ * Register middlewares.
+ */
+app.use(morgan("dev"));
 
 export { app };
