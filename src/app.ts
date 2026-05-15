1844fcb2a3e321eb22ddd7a5ebc378ed655f8650
luutanhung <luutanhung.dev@gmail.com>
2026-05-15 08:15:47 +0700

    chore: add compression to compress response bodies for all requests that traverse through the middleware

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
+import compression from "compression";
 import type { Express } from "express";
 import express from "express";
+import helmet from "helmet";
 import morgan from "morgan";
 
 const app: Express = express();
@@ -8,5 +10,11 @@ const app: Express = express();
  * Register middlewares.
  */
 app.use(morgan("dev"));
+app.use(helmet());
+app.use(
+  compression({
+    level: 9,
+  }),
+);
 
 export { app };
