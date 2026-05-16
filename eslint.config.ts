0e93919ba07262240f4fb96b3f156ee1ff2fdc0c
luutanhung <luutanhung.dev@gmail.com>
2026-05-16 08:04:24 +0700

    chore: uninstall sort-imports

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,38 +1,15 @@
 import js from "@eslint/js";
 import { defineConfig } from "eslint/config";
-import simpleSortImport from "eslint-plugin-simple-import-sort";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
 export default defineConfig([
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
-    plugins: { js, "simple-import-sort": simpleSortImport },
+    plugins: { js },
     extends: ["js/recommended"],
 
     languageOptions: { globals: globals.node },
-
-    rules: {
-      "simple-import-sort/imports": [
-        "error",
-        {
-          groups: [
-            // Node.js builtins
-            ["^node:"],
-
-            // Third-party packages
-            ["^@?\\w"],
-
-            // Absolute imports
-            ["^@/"],
-
-            // Relative imports
-            ["^\\."],
-          ],
-        },
-      ],
-      "simple-import-sort/exports": "error",
-    },
   },
   tseslint.configs.recommended,
 ]);
